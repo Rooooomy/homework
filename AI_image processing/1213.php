5ea3d6082edef8109a36d7409cc55d6aacbe558f
Romy <151701333+Rooooomy@users.noreply.github.com>
2023-12-13 10:13:44 +0800

    Add files via upload

---
+++ AI_image processing/1213.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		
+	</head>
+	<body>
+	<table width="70%" border="0.5" align="center">
+	<tr> <td> <img src="AI_image processing/第一張處理前.jpg"></td>
+	<td> <img src="AI_image processing/第一張處理後.jpeg"></td></tr>
+	<tr> <td> <img src="AI_image processing/第二張處理前.jpg"></td>
+	<td> <img src="AI_image processing/第二張處理後.jpeg"></td></tr>
+	</table>
+	</body>
+
+
+</html>
